2315d0bf0be90ba193525ca6e5bd44ab613dd54a
diaoby <183204622@qq.com>
2017-12-12 11:06:45 +0800

    commit

--- src/main/java/spring/ch08/Audience.java
+++ src/main/java/spring/ch08/Audience.java
@@ -1,5 +1,6 @@
 package spring.ch08;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 
 public class Audience {
 	
@@ -17,4 +18,14 @@ public class Audience {
 		System.out.println("===========表演失败后退票===========");
 	}
 	
+	public void watchPerformacne(ProceedingJoinPoint jp) {
+		try {
+			System.out.println("===========Around 表演前关手机===========");
+			System.out.println("===========Around 表演前就坐===========");
+			jp.proceed();
+			System.out.println("===========Around 表演后鼓掌===========");
+		} catch (Throwable e) {
+			System.out.println("===========Around 表演失败后退票===========");
+		}
+	}
 }
